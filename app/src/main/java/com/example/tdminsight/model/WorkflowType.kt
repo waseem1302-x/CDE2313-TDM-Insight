b99e2edaa4f896398c737ed677c8925999b2f9d2
Waseem Mushtaq <waseemmushtaq045@gmail.com>
2026-09-12 15:29:46 +0800

    feat: add TDM workflow types

---
+++ app/src/main/java/com/example/tdminsight/model/WorkflowType.kt
@@ -0,0 +1,7 @@
+package com.example.tdminsight.model
+
+enum class WorkflowType(val displayName: String) {
+    PRE("Vancomycin Pre"),
+    POST("Vancomycin Post"),
+    PRE_POST("Vancomycin Pre + Post")
+}
